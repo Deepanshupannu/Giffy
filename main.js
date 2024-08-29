a4493f4487253ef0266f419dd652abebc597e44a
Deepanshupannu <deepanshupannu817@gmail.com>
2024-08-29 12:16:46 +0530

    added error handling

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const img = document.querySelector('img');
+const btn=document.querySelector('button')
 const searchbox=document.querySelector("#search")
 function pagereload(){
 
   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=WHQbXetrA41lL1UxriYxa5wlZjcCXFdk&s=`+ searchbox.value, {mode: 'cors'})
     .then(function(response) {
+      if(!response.ok){
+        throw new Error("Network was not ok")
+      }
       return response.json();
     })
     .then(function(response) {
       img.src = response.data.images.original.url;
-    });
+    })
+    .catch((error)=>{
+      console.error("There is problem with fetch opertion:",error)
+    }
+    );
 }
-  const btn=document.querySelector('button')
-  pagereload()
-  btn.addEventListener("click",pagereload)
+pagereload()
+btn.addEventListener("click",pagereload)
